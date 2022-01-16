2b35e44efcfb416c2d6b037cdb6750897b5f9fd4
mydubs <my.dubs@gmail.com>
2022-01-16 17:08:35 -0600

    Updated authentication methods and hardened security for jwt tokens; added refresh token route.

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,7 @@ var jwt = require("jsonwebtoken");
 const secret = process.env.JWT_SECRET;
 const config = require("../config").getConfig;
 const resBuild = require("../shared/response").sendResponse;
+const af = require("../functions/auth"); // Auth Functions
 
 exports.test = async(function (req, res, next) {
   res.json(resBuild(true, "Test Route Works!"));
@@ -23,34 +24,63 @@ exports.create = async(function* (req, res) {
   }
 });
 
-exports.login = async(function* (req, res) {
-  User.findOne(
-    {
-      username: req.body.username,
-    },
-    function (err, user) {
-      if (err) throw err;
-      if (!user) {
-        res.status(401).json(resBuild(false, "User Not Found"));
-      } else {
-        user.authenticate(req.body.password, async function (err, match) {
-          if (match && !err) {
-            var token = await jwt.sign(
-              {
-                exp: Math.floor(Date.now() / 1000) + 60 * 60,
-                id: user.id,
-                username: user.username,
-              },
-              secret
-            );
-            res
-              .status(200)
-              .json(resBuild(true, "Successfully Logged in!", { token }));
-          } else {
-            res.status(401).json(resBuild(false, "Bad Info"));
-          }
-        });
-      }
-    }
-  );
-});
+exports.login = async function (req, res) {
+  let username = req.body.username;
+  let password = req.body.password;
+  try {
+    // Check for all required data
+    if (!username || !password) throw "Please provide all data for request";
+
+    // Check if the user exists in the db
+    let user = await User.findOne({ username: username });
+    if (!user) throw "No User Found";
+
+    // Check if Users Password is correct
+    let checkPassword = await user.authenticate(password);
+    if (checkPassword === false) throw "Bad User Information";
+
+    // Generate Token
+    let token = await af.setUserToken(user, req.socket.remoteAddress);
+    if (token === false) throw "Failed to make token";
+
+    // Send Response
+    res.status(200).json(resBuild(true, "Successfully Logged in!", { token }));
+  } catch (err) {
+    res.status(401).json(resBuild(false, err));
+  }
+};
+
+exports.refresh = async function (req, res) {
+  let token = req.headers["x-access-token"];
+  let address = req.socket.remoteAddress;
+  try {
+    if (!req.user || !token) throw "No user or token data";
+    let user = await User.findOne({ username: req.user.username });
+    if (!user) throw "User not found. Can't refresh";
+
+    // Generate new token and delete old one.
+    let newToken = await af.refreshToken(user, token, address);
+    if (newToken === false) throw "Failed to refresh token";
+
+    res
+      .status(200)
+      .json(resBuild(true, "Token Refreshed!", { token: newToken }));
+  } catch (errMessage) {
+    res.status(401).json(resBuild(false, errMessage));
+  }
+};
+
+exports.logout = async function (req, res) {
+  let token = req.headers["x-access-token"];
+  try {
+    let user = await User.findOne({ username: req.user.username });
+    if (!user) throw "User not found. Can't logout";
+
+    let killToken = await af.removeToken(user, token);
+    if (killToken === false) throw "Failed to remove token & logout";
+
+    res.status(200).json(resBuild(true, "Successfully logged out!"));
+  } catch (errMessage) {
+    res.status(401).json(resBuild(false, errMessage));
+  }
+};
---
+++ middlewares/auth.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const secret = process.env.JWT_SECRET || "devsecret";
+const resBuild = require("../shared/response").sendResponse;
+const af = require("../functions/auth");
+
+exports.verifyToken = async (req, res, next) => {
+  let token = req.headers["x-access-token"];
+  let address = req.socket.remoteAddress;
+  try {
+    // Check if token attached to header
+    if (!token) throw "No Token.";
+
+    // Check if token is expired
+    let decoded = await jwt.verify(token, secret);
+    if (decoded.expiredAt) throw "Token Expired.";
+
+    // Check username from provided token
+    let user = await User.findOne({ username: decoded.username });
+    if (!user) throw "Bad token data.";
+
+    // Check if token is active & origin
+    let isTokenActive = await af.checkActiveToken(user, token, address);
+    if (isTokenActive === false) throw "Inactive token.";
+
+    // Attached decoded info to req.user object
+    req.user = decoded;
+    next();
+  } catch (errMessage) {
+    if (!errMessage) errMessage = "Default error.";
+    if (errMessage.expiredAt) errMessage = "Token Expired";
+    return res.status(400).json(resBuild(false, errMessage));
+  }
+};
